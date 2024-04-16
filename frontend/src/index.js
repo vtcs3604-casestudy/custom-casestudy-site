9604ac2a7785cf394976975b27d680e9ba67af00
runnerjoe28 <osuchjm@gmail.com>
2024-04-15 21:15:43 -0400

    Initial

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PublicClientApplication, EventType } from '@azure/msal-browser';
+import { msalConfig } from './config';
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
+  msalInstance.setActiveAccount(msalInstance.getActiveAccount()[0])
+  console.log("Setting AD happended")
+}
+
+msalInstance.addEventCallback((message) => {
+  if (message.eventType === EventType.LOGIN_SUCCESS || message.eventType === EventType.INITIALIZE_START) {
+    const account = message.paylod.account;
+    msalInstance.setActiveAccount(account);
+    console.log("Login Success")
+  } else {
+    console.log(message)
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App instance={msalInstance}/>
   </React.StrictMode>
 );
 
