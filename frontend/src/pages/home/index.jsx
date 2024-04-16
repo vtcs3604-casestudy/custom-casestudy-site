d794bc10ddb5f4c8b66359bf57aaa5b9c0949d6e
runnerjoe28 <osuchjm@gmail.com>
2024-04-15 23:57:27 -0400

    Rework page logic to setup routing

---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { AuthenticatedTemplate, useMsal, UnauthenticatedTemplate } from '@azure/msal-react';
+import { Container } from 'react-bootstrap';
+import { NavigationBar } from '../../components/NavigationBar';
+import { IdTokenData } from '../../components/DataDisplay';
+
+/**
+ * Creates the homepage for the website
+ */
+export const Home = () => {
+    const { instance } = useMsal();
+
+    let activeAccount;
+
+    if (instance) {
+        activeAccount = instance.getActiveAccount();
+        console.log(activeAccount)
+    }
+    
+    return (
+        <div>
+            <NavigationBar />
+                <br />
+                <h5>
+                    <center>Welcome to the Microsoft Authentication Library For React Tutorial</center>
+                </h5>
+                <AuthenticatedTemplate>
+                    <p><center>You are logged in {activeAccount ? activeAccount.username : 'Unknown'}</center></p>
+                </AuthenticatedTemplate>
+                <br />
+                <AuthenticatedTemplate>
+                    {activeAccount ? (
+                        <Container>
+                            <IdTokenData idTokenClaims={activeAccount.idTokenClaims} />
+                        </Container>
+                    ) : null}
+                </AuthenticatedTemplate>
+                <UnauthenticatedTemplate>
+                    <h5 className="card-title">Please sign-in to see your profile information.</h5>
+                </UnauthenticatedTemplate>
+                <br />
+                <AuthenticatedTemplate>
+                    <footer>
+                        <center>
+                            How did we do?
+                            <a
+                                href="https://forms.office.com/Pages/ResponsePage.aspx?id=v4j5cvGGr0GRqy180BHbR73pcsbpbxNJuZCMKN0lURpUMlRHSkc5U1NLUkxFNEtVN0dEOTFNQkdTWiQlQCN0PWcu"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                {' '}
+                                Share your experience!
+                            </a>
+                        </center>
+                    </footer>
+                </AuthenticatedTemplate>
+        </div>
+    )
+}
