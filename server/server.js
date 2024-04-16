7b5abf92c22f31c0002d16f05aa2bfd976de7823
Hamza Jaldi <hamzajaldi@Hamzas-MacBook-Air.local>
2024-04-16 10:39:31 -0400

    Mongo First Import

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/editProfile')
 
 
@@ -15,9 +16,15 @@ app.use((req, res, next) => {
 })
 
 // routes (set path at /api)
-app.use('/api/editProfile', workoutRoutes)
+//app.use('/api/editProfile', workoutRoutes)
 
-// listen to requests
-app.listen(process.env.PORT, ()=> {
-  console.log('Listening on port', process.env.PORT)
-})
+// connect to mongoose
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, ()=> {
+      console.log('connected to db and listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
