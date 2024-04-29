5b4787bd83ce6955bde67041acea95643f6371e6
denaria <denaria@vt.edu>
2024-04-28 21:41:43 -0400

    Changes to editprofile

--- frontend/src/pages/editprofile/index.jsx
+++ frontend/src/pages/editprofile/index.jsx
@@ -1,56 +1,98 @@
-import { React, useEffect, useRef, useState } from 'react';
-import { AuthenticatedTemplate, useMsal, UnauthenticatedTemplate } from '@azure/msal-react';
+import { React, useEffect, useState } from 'react';
 import './editprofile.css'
+import { useParams } from 'react-router-dom';
+import API_HOSTNAME from '../../config';
 
 export const EditProfile = () => {
+    const {username} = useParams();
+    const [userData, setUserData] = useState({});
     const [title, setTitle] = useState("");
-    const [tags, setTags] = useState('');
+    const [tags, setTags] = useState([]);
     const [file, setFile] = useState(null);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    useEffect( () => {
+        fetchUserData();
+    }, []
+)
+    
+
+    const fetchUserData = async() => {
+        try {
+            const response = await fetch(`${API_HOSTNAME}/api/profile/${username}`);
+            const parsedUserData = await response.json();
+            setUserData(parsedUserData);
+            setTitle(parsedUserData.title); // Set title from fetched data
+            setTags(parsedUserData.tags); // Set tags from fetched data
+            setFile(parsedUserData.file)
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
     };
 
     const handleCancel = () => {
 
     };
 
-    const handleAddTag = () => {
-
+    const handleAddTag = async() => {
+        
     }
+
+    // Function to handle removing a tag
+    const handleRemoveTag = (index) => {
+        
+    };
 
     return (
         <>
             <div className="edit-container">
                 <h5>Welcome to the Edit Profile page</h5>
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor="title">Title</label>
-                    <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    required/>
-                    <br /><br />
-                    <label htmlFor="tags">Tags</label>
-                    <input
+                    <div className='saved-title'>
+                        <center>
+                        <h4><strong>Title:&nbsp;</strong>{title}</h4>
+                        </center>
+                    </div>
+                    <div className="title-wrapper">
+                        <label htmlFor="title">Title</label>
+                        <input
                         type="text"
-                        id="tags"
-                        name="tags"
-                        value={tags}
-                        onChange={(e) => setTags(e.target.value)}
-                    />
-                    <button type="button" onClick={handleAddTag}>Add Tag</button>
-                    <br /><br />
-                    <label htmlFor="file">Upload File:</label>
-                    <input
-                        type="file"
-                        id="file"
-                        name="file"
-                        onChange={(e) => setFile(e.target.files[0])}
-                    />
-                    <br /><br />
+                        id="title"
+                        name="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required/>
+                        <br /><br />
+                    </div>
+                    <div className="tags">
+                        <label htmlFor="tags">Tags</label>
+                        <input
+                            type="text"
+                            id="tags"
+                            name="tags"
+                            value={tags}
+                            onChange={(e) => setTags(e.target.value)}
+                        />
+                        <button type="button" onClick={handleAddTag}>Add Tag</button>
+                        <div className="tag-wrapper">
+                        
+                        </div>
+                        <br /><br />
+                    </div>
+                    <div className="upload"> 
+                        <label htmlFor="file">Upload File:</label>
+                        <input
+                            type="file"
+                            id="file"
+                            name="file"
+                            onChange={(e) => setFile(e.target.files[0])}
+                        />
+                        <br /><br />
+                    </div>
                     <div className="save-cancel-wrapper">
                         <button className="save-btn" type="submit">SAVE</button>
                         <button className="cancel-btn" type="button" onClick={handleCancel}>CANCEL</button>
